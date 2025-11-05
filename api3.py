8764c3a8ebc745e7bf71316dbcede1b069354ab7
GabrielVieira-Code <128522101+GabrielVieira-Code@users.noreply.github.com>
2025-11-05 08:47:28 -0300

    Refactor to handle multiple students in a list

--- api3.py
+++ api3.py
@@ -1,15 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-aluno={
+alunos=[{
     'nome':'Gabriel',
     'idade':26,
     'telefone':'12312312'
+},
+{
+    'nome':'fernando',
+    'idade':12,
+    'telefone':'2312322'
 }
+]
 
+# caminho a ser seguido pelo ususario
 @app.route('/')
-def home():
-    
-    return render_template('register.html',aluno=aluno)
+def index():
+    return render_template('register.html',alunos=alunos)
+
+@app.route('/adicionar',methods=['POST'])
+def adicioar():
+    nome = request.form['nome']
+    idade = request.form['idade']
+  
+ 
+    alunos.append( {'nome':nome,'idade':idade})
+    return redirect(url_for('index'))
+
+
+
 
 app.run(debug=True)
